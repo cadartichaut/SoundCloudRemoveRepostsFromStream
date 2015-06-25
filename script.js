f7a3932de1703da93b2191ed1980c5e8c09950f1
Matthieu Cadart <matthieu.cadart@fontyou.com>
2015-06-25 12:58:02 +0200

    added script

---
+++ script.js
@@ -0,0 +1,27 @@
+(function() {    var skipButton = document.getElementsByClassName("skipControl playControls__icon sc-ir skipControl__next")[0];
+    var skip = [];
+
+    function repeat() {
+        var posts = document.getElementsByClassName('soundList__item');
+        for (var i = 0; i < posts.length; i++) {
+            if (posts[i].innerHTML.indexOf('Reposted') !== -1) {
+                skip.push(posts[i].getElementsByClassName("soundTitle__title")[0].getElementsByTagName('span')[0].innerHTML);
+                posts[i].parentNode.removeChild(posts[i]);
+            }
+        }
+        var check = document.getElementsByClassName("playbackSoundBadge__titleContextContainer")[0];
+        console.log(skip);
+        if (check !== undefined) {
+            var curSongHolder = check.getElementsByTagName('a')[1];
+            for (var i = 0; i < skip.length; i++) {
+                if (skip[i] === curSongHolder.innerHTML) {
+                    console.log('gah');
+                    skipButton.click();
+                }
+            }
+        }
+        console.log('run');
+        setTimeout(repeat, 300);
+    }
+    repeat();
+})();
